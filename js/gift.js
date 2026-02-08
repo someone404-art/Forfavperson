ae01bcdd03b718be0a8ed8bd2232f7bbcfca7a5e
someone404-art <nestleokta@gmail.com>
2026-02-08 21:33:24 +0700

    Create gift.js

---
+++ js/gift.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8" />
+  <title>Sedikit Cerita</title>
+
+  <link rel="stylesheet" href="css/base.css" />
+  <link rel="stylesheet" href="css/theme-polaroid.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  
+  <div class="confetti-bg"></div>
+  
+<div class="polaroid-card">
+
+  <!-- SLOT FOTO -->
+  <div class="photo-slot">
+    <img src="assets/photo.jpg" alt="memory photo">
+  </div>
+
+  <h3 class="memory-title">memory</h3>
+  <p id="typing"></p>
+
+  <button id="nextBtn" class="hidden next-btn">lanjut pelan →</button>
+</div>
+  
+<audio id="bgMusic" src="assets/music1.mp3" loop></audio>
+
+<script src="js/gift.js"></script>
+</body>
+</html>
